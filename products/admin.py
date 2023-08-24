dac0dcd0ecb5bb6e1f0ae7f8f339969f495851c3
zbari023 <barry450643@gmail.com>
2023-08-24 20:36:22 +0200

    add the first class model with test product

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    list_filter = ['name', 'price']
+    search_fields = ['name', 'price']
+
+admin.site.register(Products, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='products')
+    price = models.FloatField()
+    
+    
+    def __str__(self):
+        return self.name
